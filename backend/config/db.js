6206c9b4c8933772ceb741ee5127e201be0131e0
Asmit Katiyar <asmitkatiyar33@gmail.com>
2025-06-30 23:12:43 +0530

    schemas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 const connectDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("DB connected")
-    } catch (error) {
-        console.log("DB error")
-    }
-    
-}
-export default connectDb
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("DB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default connectDb;
