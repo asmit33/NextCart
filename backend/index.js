acf2b27c1775951fc1460d7dcbbbe03a27988a7f
asmit33 <142411946+asmit33@users.noreply.github.com>
2025-07-21 22:13:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["papaya-sfogliatella-4f9f9d.netlify.app", "http://localhost:5174"],
     credentials: true,
   })
 );
